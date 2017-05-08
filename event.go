f9b15d2927e9fb0bc6f2f634a65c7582fc3fad07
myafeier <hiall@aliyun.com>
2017-05-08 17:29:23 +0800

    fix response of event

--- event.go
+++ event.go
@@ -3,10 +3,10 @@ package om
 import "encoding/xml"
 
 type Event struct {
-	XMLName   xml.Name      `xml:"Event"`
-	Attribute string        `xml:"attribute,attr"`
-	Exts      []*ExtBasic   `xml:"ext,omitempty"`
-	Visitor   *VisitorBasic `xml:"visitor,omitempty"`
-	Menu      *Menu         `xml:"menu,omitempty"`
-	Outer     *Outer        `xml:"outer,omitempty"`
+	XMLName   xml.Name       `xml:"Event"`
+	Attribute string         `xml:"attribute,attr"`
+	Exts      []*ExtBasic    `xml:"ext,omitempty"`
+	Visitor   *VisitorBasic  `xml:"visitor,omitempty"`
+	Menu      *Menu          `xml:"menu,omitempty"`
+	Outer     *OuterResponse `xml:"outer,omitempty"`
 }
